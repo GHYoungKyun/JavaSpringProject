83cedf88522451af0e697d1997725009e2b1f2e8
GHYoungKyun <dudrbsapdlf@naver.com>
2023-06-22 22:46:11 +0900

    mysql connect

--- src/main/java/com/example/javaspringproject/dbconnector/Post.java
+++ src/main/java/com/example/javaspringproject/dbconnector/Post.java
@@ -3,8 +3,10 @@ package com.example.javaspringproject.dbconnector;
 import lombok.Data;
 @Data
 public class Post {
+    private int postIdx;
     private char title;
     private char name;
     private char date;
     private String content;
+    private boolean delidx;
 }
--- src/main/java/me/ghyk/demo/mapper/PostMapper.java
+++ src/main/java/me/ghyk/demo/mapper/PostMapper.java
@@ -1,10 +1,12 @@
 package me.ghyk.demo.mapper;
 
 import com.example.javaspringproject.dbconnector.Post;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
+
 @Mapper
-public interface UserMapper {
+public interface PostMapper {
 
     List<Post> findAll();
 }
